5f854b931b1988a698a3970602c0846745a486ce
cauerrds <caue_rrds@hotmail.com>
2023-06-21 03:44:10 -0300

    fix: leaflet import

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -2,22 +2,27 @@
 
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { icon } from "leaflet"
 import 'leaflet/dist/leaflet.css';
 
 
 export default function Map() {
+const ICON = icon({
+  iconUrl: "../../../mark.png",
+  iconSize: [32, 32],
+})
 
     return (
     <MapContainer center={[-26.3553, -52.8495]} zoom={10} style={{ height: '400px', width: '100%', maxWidth: '400px' }}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-      <Marker position={[-26.3553, -52.8495]}>
+      <Marker icon={ICON} position={[-26.3553, -52.8495]}>
         <Popup>
           São Lourenço do Oeste, SC
         </Popup>
       </Marker>
 
-      <Marker position={[-26.2292, -52.6704]}>
+      <Marker icon={ICON} position={[-26.2292, -52.6704]}>
         <Popup>
           Pato Branco, PR
         </Popup>
